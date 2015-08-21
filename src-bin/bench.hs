60ff4417a4bdba32ae2de9be5bde41fead76682e
Oliver Batchelor <saulzar@gmail.com>
2015-08-21 22:35:08 +1200

    Fix bugs in benchmarks, add groups of 25/50

--- src-bin/bench.hs
+++ src-bin/bench.hs
@@ -20,9 +20,6 @@ import Data.FileEmbed
 import Criterion
 import Criterion.Main
 
-import Reflex
-import Reflex.Dom
-
 import System.Exit
 
 
@@ -32,29 +29,31 @@ main :: IO ()
 main = mainWidgetWithCss $(embedFile "style.css") benchmarks
 
 
+type TaskMap = Map Int Todo.Task
+
+initialTasks :: Int -> TaskMap
+initialTasks n = Map.fromList $ map (\i -> (i,  Todo.Task ("task " ++ show i) True)) [1..n]
 
 
-
-todoWithHooks :: MonadWidget t m => Event t (Map Int Todo.Task) ->  Event t (Map Int Todo.Task) -> Dynamic t Todo.Filter -> m (Event t Int)
-todoWithHooks addTask removeTask activeFilter = do
+todoWithHooks :: MonadWidget t m => TaskMap -> Event t TaskMap ->  Event t TaskMap -> Dynamic t Todo.Filter -> m (Event t Int)
+todoWithHooks initial addTask removeTask activeFilter = do
   
     rec 
-      tasks <- foldDyn ($) mempty $ mergeWith (.)
+      tasks <- foldDyn ($) initial $ mergeWith (.)
                     [ mappend <$> addTask 
                     , flip Map.difference <$> removeTask
+                    , listModifyTasks
                     ]
                     
       (listModifyTasks, itemsAdded) <- Todo.taskList activeFilter tasks
-    return itemsAdded    
+      counter <- updated <$> count (void itemsAdded)
+    return counter    
     
     
 testAdd :: forall t m. MonadWidget t m => Int -> m (Event t ())
 testAdd n = do
   rec
-    tick <- todoWithHooks addTask never (constDyn Todo.All)
-    
-    postBuild <- getPostBuild
-    counter <- updated <$> count (leftmost [void tick, postBuild])
+    counter <- todoWithHooks mempty addTask never (constDyn Todo.All)
         
     let addTask = newTask <$> (ffilter (< n)) counter
   return $ void $ ffilter (>= n) counter    
@@ -66,36 +65,23 @@ testAdd n = do
 testFilter :: forall t m. MonadWidget t m => Int -> Int -> m (Event t ())
 testFilter n repeats = do
   rec
-    postBuild <- getPostBuild
-    tick <- todoWithHooks (taskList <$ postBuild) never filter
-        
-    counter <- updated <$> count (void tick)
+    counter <- todoWithHooks (initialTasks n) never never filter
     filter <- holdDyn Todo.Active (filterNum <$> counter)
-        
   return $ void $ ffilter (>= repeats) counter    
   
   where      
-    taskList = Map.fromList $ map (\i -> (i,  Todo.Task ("task " ++ show i) True)) [1..n]
     filterNum i = if odd i then Todo.Active else Todo.Completed
+
+testCreate :: forall t m. MonadWidget t m => Int -> m (Event t ())
+testCreate n = void <$> todoWithHooks (initialTasks n) never never (constDyn Todo.All)
     
     
 testModify :: forall t m. MonadWidget t m => Int -> m (Event t ())
 testModify n = do
   rec
-    postBuild <- getPostBuild
-    tick <- todoWithHooks addEvent never (constDyn Todo.All)
-        
-    counter <- updated <$> count (void tick)
-    let addEvent = leftmost 
-          [ taskList <$ postBuild
-          , modifyTask <$> counter ]
-        
+    counter <- todoWithHooks (initialTasks n) (modifyTask <$> counter) never (constDyn Todo.All)
   return $ void $ ffilter (>= n) counter    
-  
   where      
-    taskList = Map.fromList $ map (\i -> (i,  Todo.Task ("task " ++ show i) True)) [1..n]
-    filterNum i = if odd i then Todo.Active else Todo.Completed    
-    
     modifyTask i = Map.singleton i (Todo.Task ("task " ++ show i) False)
 
 
@@ -107,7 +93,6 @@ newEventWithFire =  do
   
   (e, triggerRef) <- newEventWithTriggerRef
   return (e, \a -> postGui $ mapM_ (\t -> runWithActions [t :=> a]) =<< readRef triggerRef)
-
 
 
 benchmarks :: forall t m. MonadWidget t m => m ()
@@ -125,16 +110,19 @@ benchmarks = do
       fireTest test
       void $ takeMVar doneVar
 
+    benchN n = bgroup ("n = " ++ show n) $
+      [ bench "create items" $ nfIO $ runTest (testCreate n)      
+      , bench "add items (incremental)" $ nfIO $ runTest (testAdd n)
+      , bench "modify items (incremental)" $ nfIO $ runTest (testModify n)
+      , bench "toggle filter (x10)" $ nfIO $ runTest (testFilter n 10)
+      ]
 
   void $ liftIO $ forkIO $ do 
     defaultMain 
       [ bench "creation" $ nfIO $ runTest (Todo.todoMVC >> getPostBuild)
-      , bench "add100" $ nfIO $ runTest (testAdd 100)
-      , bench "modify100" $ nfIO $ runTest (testModify 100)
-
-      , bench "filter100 x 50" $ nfIO $ runTest (testFilter 100 50)
+      , benchN 25
+      , benchN 50
       ]
-
+      
     Dom.postGUIAsync $ exitWith ExitSuccess
-
   
