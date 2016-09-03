c6224e777c794b4cd0b17381b53a23328fe63aeb
David Johnson <djohnson.m@gmail.com>
2016-09-03 15:45:09 -0500

    Updated nix, added README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# TodoMVC in Reflex - [Try It!](http://financial-controller-dolly-10433.bitballoon.com/)
+
+All of the code lives in `src/Reflex/TodoMVC.hs`.
+`style.css` is embedded into the application.
+
+## Build Instructions
+
+```bash
+nix-build
+cd result/bin/reflex-todomvc.jsexe
+```
+
+Then open `index.html` in your browser!
--- default.nix
+++ default.nix
@@ -1,16 +1,3 @@
-{ mkDerivation, reflex, reflex-dom, file-embed
-}:
-
-mkDerivation {
-  pname = "reflex-todomvc";
-  version = "0.1";
-  src = builtins.filterSource (path: type: baseNameOf path != ".git") ./.;
-  isExecutable = true;
-  isLibrary = true;
-  buildDepends = [
-    reflex
-    reflex-dom
-    file-embed
-  ];
-  license = null;
-}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghcjs" }:
+	    nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./reflex-todomvc.nix
+	   { }
