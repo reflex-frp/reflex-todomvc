4282c21cef92f7af2a8a90bdabfe2980e0697835
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2017-01-20 18:28:37 +1300

    Add jsaddle-warp, -wkwebview & -webkit2gtk to nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, reflex, reflex-dom, file-embed, cabal-macosx
+{ mkDerivation, reflex, reflex-dom, file-embed, cabal-macosx, jsaddle-warp, jsaddle-webkit2gtk, jsaddle-wkwebview, ghc, stdenv
 }:
 
 mkDerivation {
@@ -12,6 +12,14 @@ mkDerivation {
     reflex-dom
     file-embed
     cabal-macosx
-  ];
+  ] ++ (if ghc.isGhcjs or false then [
+  ] else if stdenv.isDarwin then [
+    jsaddle-wkwebview
+#    jsaddle-webkit2gtk
+    jsaddle-warp
+  ] else [
+    jsaddle-webkit2gtk
+    jsaddle-warp
+  ]);
   license = null;
 }
